43b1e5c8b4ae8466f55a857d97a21c403e8310cf
FelipeGabrill <felipegabrielodss@gmail.com>
2025-05-09 21:53:53 -0300

    TypeScript: Interfaces

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,10 @@
-import { Component, computed, Input, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -7,14 +13,14 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({  required: true  }) avatar!: string;
-  @Input({  required: true  }) name!: string;
+  @Input( {required: true} ) user!: User;
+  @Output() select = new EventEmitter();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar
+    return 'assets/users/' + this.user.avatar
   }
 
   onSelectUser() {
-    
+    this.select.emit(this.user.id);
   }
 }

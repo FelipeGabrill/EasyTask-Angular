622e13d939e1b52533b2dbcaa9421372776ea001
FelipeGabrill <felipegabrielodss@gmail.com>
2025-05-11 21:21:03 -0300

    Tasks Service

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from "./new-task/new-task.component";
 import { NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -14,54 +15,23 @@ export class TasksComponent {
   @Input({ required: true }) name!: string;
   isAddingTask = false;
 
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Build first prototype',
-      summary: 'Build first prototype of the online shop.',
-      dueDate: '2025-05-21'
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Master Angular',
-      summary: 'Learn all the basic and advanced features of Angular.',
-      dueDate: '2025-04-20'
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary: 'Prepare issue template which will help with project management.',
-      dueDate: '2025-07-12'
-    }
-  ]
+  constructor(private tasksService: TasksService) {
+    this.tasksService = tasksService;
+  }
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId);
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.tasksService.removeTask(id);
   }
 
   onStartAddTask() {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask() {
-    this.isAddingTask = false;
-  }
-
-  onAddTask(taskData: NewTaskData) {
-    this.tasks.push({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date
-    })
+  onCloseAddTask() {
     this.isAddingTask = false;
   }
 }

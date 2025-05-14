32722c8909ca8d4f3ed4718ab906d78b8da882df
FelipeGabrill <felipegabrielodss@gmail.com>
2025-05-14 01:32:56 -0300

    Bootstrapping Apps with Angular Modules

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { TasksComponent } from "./tasks/tasks.component";
+
+@NgModule({
+    declarations: [AppComponent],
+    bootstrap: [AppComponent],
+    imports: [BrowserModule ,HeaderComponent, UserComponent, TasksComponent]
+})
+export class AppModule {
+
+}
